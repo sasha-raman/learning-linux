0171b7f3ae36abb56c963025fb9946175d9c8b64
Sasha Raman <sasha.raman07@gmail.com>
2026-09-15 19:02:47 -0700

    Update from chapter 2.

---
+++ chapter02/kc.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+mkdir -p /tmp/chapter02
+cd /tmp/chapter02
+ls -l .
+ls -l /var/log
+cat /home/sashr/workspace/r-practice/test.R
+touch file1 file2 file3
+ls -a home/sashr/workspace/learning-linux
+mkdir fsociety
